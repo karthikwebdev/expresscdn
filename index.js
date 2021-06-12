1067aaa6633f490cfeaea926612e664e03c30db9
karthik enumarthi <kartheekenumarthi@gmail.com>
2021-06-12 19:31:31 +0530

    removed cors policy

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require("express");
+const cors = require("cors");
 const path = require("path");
 
 const app = express()
-
+app.use(cors())
 app.use(express.static("files"))
 
 app.listen(process.env.PORT,console.log("Server started"))
